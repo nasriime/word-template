b57d1f87d789961210400f8e4d2477f43661f2a9
Mohamed  NasrAllah <mnasr4040@gmail.com>
2019-09-10 18:20:26 +0200

    page1

--- before.js
+++ before.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const {  Document, Footer, Header, Media, Packer, Paragraph, TextRun, 
     HorizontalPositionAlign, VerticalPositionAlign, 
     HorizontalPositionRelativeFrom, VerticalPositionRelativeFrom,
-    AlignmentType} = require("docx");
+    AlignmentType, HeadingLevel, Table, WidthType, VerticalAlign} = require("docx");
 
 const doc = new Document(undefined, {
     top: 0,
@@ -20,10 +20,7 @@ const image1 = Media.addImage(doc, fs.readFileSync("./bg2.jpg"),50,50, {
         verticalPosition: {
             relative: VerticalPositionRelativeFrom.OUTSIDE_MARGIN,
             align: VerticalPositionAlign.TOP,
-        },
-        margins: {
-            bottom: 201440,
-        },
+        }
     },
 });
 
@@ -36,13 +33,205 @@ const image2 = Media.addImage(doc, fs.readFileSync("./bg2.jpg"),50,50, {
         verticalPosition: {
             relative: VerticalPositionRelativeFrom.OUTSIDE_MARGIN,
             align: VerticalPositionAlign.TOP,
-        },
-        margins: {
-            bottom: 201440,
-        },
+        }
     },
 });
 
+const image3 = Media.addImage(doc, fs.readFileSync("./bg2.jpg"),50,50, {
+    // floating: {
+    //     horizontalPosition: {
+    //         relative: HorizontalPositionRelativeFrom.OUTSIDE_MARGIN,
+    //         align: HorizontalPositionAlign.RIGHT
+    //     },
+    //     verticalPosition: {
+    //         relative: VerticalPositionRelativeFrom.OUTSIDE_MARGIN,
+    //         align: VerticalPositionAlign.TOP,
+    //     },
+    //     margins: {
+    //         bottom: 201440,
+    //     },
+    // },
+});
+
+const image4 = Media.addImage(doc, fs.readFileSync("./bg2.jpg"),50,50, {
+    // floating: {
+    //     horizontalPosition: {
+    //         relative: HorizontalPositionRelativeFrom.OUTSIDE_MARGIN,
+    //         align: HorizontalPositionAlign.LEFT
+    //     },
+    //     verticalPosition: {
+    //         relative: VerticalPositionRelativeFrom.OUTSIDE_MARGIN,
+    //         align: VerticalPositionAlign.TOP,
+    //     },
+    //     margins: {
+    //         bottom: 201440,
+    //     },
+    // },
+});
+
+const page1Table = new Table({
+    rows: 7,
+    columns: 6,
+    width: 100,
+    widthUnitType: WidthType.PERCENTAGE,
+});
+
+    page1Table.getRow(0).mergeCells(0, 1);
+    page1Table.getRow(0).mergeCells(2, 3);
+
+    page1Table.getRow(1).mergeCells(0, 1);
+    page1Table.getRow(1).mergeCells(2, 3);
+
+    page1Table.getRow(2).mergeCells(0, 1);
+    page1Table.getRow(2).mergeCells(2, 3);
+
+    page1Table.getRow(5).mergeCells(0, 1);
+    page1Table.getRow(5).mergeCells(2, 3);
+
+    page1Table.getRow(6).mergeCells(0, 1);
+    page1Table.getRow(6).mergeCells(2, 3);
+
+    page1Table.getColumn(0).mergeCells(3, 4);
+    page1Table.getColumn(2).mergeCells(0, 1);
+
+    // column 0
+    page1Table
+    .getCell(0, 0)
+    .add(new Paragraph("Optimization Cluster No.:"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(1, 0)
+    .add(new Paragraph("1,0"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(2, 0)
+    .add(new Paragraph("2,0"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(3, 0)
+    .add(new Paragraph("3,0"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(5, 0)
+    .add(new Paragraph("5,0"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(6, 0)
+    .add(new Paragraph("6,0"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+
+    //column 1
+    page1Table
+    .getCell(3, 1)
+    .add(new Paragraph("3,1"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(4, 1)
+    .add(new Paragraph("4,1"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+
+    // column 2
+    page1Table
+    .getCell(0, 1)
+    .add(new Paragraph("0,1"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(1, 1)
+    .add(new Paragraph("1,1"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(2, 1)
+    .add(new Paragraph("2,1"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(3, 2)
+    .add(new Paragraph("3,2"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(4, 2)
+    .add(new Paragraph("4,2"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(5, 1)
+    .add(new Paragraph("5,1"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(6, 1)
+    .add(new Paragraph("6,1"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+
+    // column 2
+    page1Table
+    .getCell(0, 2)
+    .add(new Paragraph("0,2"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(1, 2)
+    .add(new Paragraph("1,2"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(2, 2)
+    .add(new Paragraph("2,2"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(5, 2)
+    .add(new Paragraph("5,2"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(6, 2)
+    .add(new Paragraph("6,2"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+
+    // column 3
+    page1Table
+    .getCell(0, 3)
+    .add(new Paragraph("0,3"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(1, 3)
+    .add(new Paragraph("1,3"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(2, 3)
+    .add(new Paragraph("2,3"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(3, 3)
+    .add(new Paragraph("3,3"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(4, 3)
+    .add(new Paragraph("4,3"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(5, 3)
+    .add(new Paragraph("5,3"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+    page1Table
+    .getCell(6, 3)
+    .add(new Paragraph("6,3"))
+    .setVerticalAlign(VerticalAlign.CENTER);
+
+     // column 4
+     page1Table
+     .getCell(3, 4)
+     .add(new Paragraph("3,4"))
+     .setVerticalAlign(VerticalAlign.CENTER);
+     page1Table
+     .getCell(4, 4)
+     .add(new Paragraph("4,4"))
+     .setVerticalAlign(VerticalAlign.CENTER);
+     
+
+      //column 5
+      page1Table
+      .getCell(3, 5)
+      .add(new Paragraph("3,5"))
+      .setVerticalAlign(VerticalAlign.CENTER);
+      page1Table
+      .getCell(4, 5)
+      .add(new Paragraph("4,5"))
+      .setVerticalAlign(VerticalAlign.CENTER);
 
 doc.addSection({
     headers: {
@@ -54,12 +243,7 @@ doc.addSection({
                 new Paragraph(""),
                 new Paragraph(""),
                 new Paragraph({
-                    children:[
-                        new TextRun({
-                            text: "Github is the best",
-                        })
-                    ],
-                    // text: "Short hand notation for adding text.",
+                    text: "Short hand notation for adding text.",
                     alignment: AlignmentType.CENTER,
                     thematicBreak: true,
                 }),
@@ -93,7 +277,19 @@ doc.addSection({
         new Paragraph(""),
         new Paragraph(""),
         new Paragraph(""),
-        new Paragraph("Hello World"),
+        new Paragraph({
+            children: [image3, image4],
+            alignment: AlignmentType.CENTER,
+        }),
+        new Paragraph({
+            text: "Cluster Acceptance Report",
+            alignment: AlignmentType.CENTER,
+            heading	: HeadingLevel.TITLE
+        }),
+        new Paragraph(""),
+        new Paragraph(""),
+        new Paragraph(""),
+        page1Table
     ],
 });
 
--- template.js
+++ template.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
-const { Document, Packer, Paragraph, Table, TextRun } = require("docx");
+const {  Document, Footer, Header, Media, Packer, Paragraph, TextRun, 
+    HorizontalPositionAlign, VerticalPositionAlign, 
+    HorizontalPositionRelativeFrom, VerticalPositionRelativeFrom,
+    AlignmentType, HeadingLevel, Table, WidthType, VerticalAlign} = require("docx");
 
 const obj={
     page1:{
@@ -7,11 +10,10 @@ const obj={
         image2Url: './bg2.jpg',
         headerText: 'LTC Header',
         footerText: 'LTC Footer',
-        bodyText: 'LTC Body'
+        bodyText: 'Cluster Acceptance Report',
+        date: '2019-09-10'
     },
-    page2:{
-
-    }
+    page2:{}
 }
 
 const create = (obj)=>{
@@ -29,10 +31,7 @@ const create = (obj)=>{
             verticalPosition: {
                 relative: VerticalPositionRelativeFrom.OUTSIDE_MARGIN,
                 align: VerticalPositionAlign.TOP,
-            },
-            margins: {
-                bottom: 201440,
-            },
+            }
         },
     });
     
@@ -45,12 +44,202 @@ const create = (obj)=>{
             verticalPosition: {
                 relative: VerticalPositionRelativeFrom.OUTSIDE_MARGIN,
                 align: VerticalPositionAlign.TOP,
-            },
-            margins: {
-                bottom: 201440,
-            },
+            }
         },
     });
+
+    const image3 = Media.addImage(doc, fs.readFileSync("./bg2.jpg"),50,50, {
+        // floating: {
+        //     horizontalPosition: {
+        //         relative: HorizontalPositionRelativeFrom.OUTSIDE_MARGIN,
+        //         align: HorizontalPositionAlign.RIGHT
+        //     },
+        //     verticalPosition: {
+        //         relative: VerticalPositionRelativeFrom.OUTSIDE_MARGIN,
+        //         align: VerticalPositionAlign.TOP,
+        //     },
+        //     margins: {
+        //         bottom: 201440,
+        //     },
+        // },
+    });
+    
+    const image4 = Media.addImage(doc, fs.readFileSync("./bg2.jpg"),50,50, {
+        // floating: {
+        //     horizontalPosition: {
+        //         relative: HorizontalPositionRelativeFrom.OUTSIDE_MARGIN,
+        //         align: HorizontalPositionAlign.LEFT
+        //     },
+        //     verticalPosition: {
+        //         relative: VerticalPositionRelativeFrom.OUTSIDE_MARGIN,
+        //         align: VerticalPositionAlign.TOP,
+        //     },
+        //     margins: {
+        //         bottom: 201440,
+        //     },
+        // },
+    });
+
+    const page1Table = new Table({
+        rows: 7,
+        columns: 6,
+        width: 100,
+        widthUnitType: WidthType.PERCENTAGE,
+    });
+    
+        page1Table.getRow(0).mergeCells(0, 1);
+        page1Table.getRow(0).mergeCells(2, 3);
+    
+        page1Table.getRow(1).mergeCells(0, 1);
+        page1Table.getRow(1).mergeCells(2, 3);
+    
+        page1Table.getRow(2).mergeCells(0, 1);
+        page1Table.getRow(2).mergeCells(2, 3);
+    
+        page1Table.getRow(5).mergeCells(0, 1);
+        page1Table.getRow(5).mergeCells(2, 3);
+    
+        page1Table.getRow(6).mergeCells(0, 1);
+        page1Table.getRow(6).mergeCells(2, 3);
+    
+        page1Table.getColumn(0).mergeCells(3, 4);
+        page1Table.getColumn(2).mergeCells(0, 1);
+    
+        // column 0
+        page1Table
+        .getCell(0, 0)
+        .add(new Paragraph("Optimization Cluster No.:"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(1, 0)
+        .add(new Paragraph("1,0"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(2, 0)
+        .add(new Paragraph("2,0"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(3, 0)
+        .add(new Paragraph("3,0"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(5, 0)
+        .add(new Paragraph("5,0"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(6, 0)
+        .add(new Paragraph("6,0"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+    
+        //column 1
+        page1Table
+        .getCell(3, 1)
+        .add(new Paragraph("3,1"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(4, 1)
+        .add(new Paragraph("4,1"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+    
+        // column 2
+        page1Table
+        .getCell(0, 1)
+        .add(new Paragraph("0,1"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(1, 1)
+        .add(new Paragraph("1,1"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(2, 1)
+        .add(new Paragraph("2,1"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(3, 2)
+        .add(new Paragraph("3,2"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(4, 2)
+        .add(new Paragraph("4,2"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(5, 1)
+        .add(new Paragraph("5,1"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(6, 1)
+        .add(new Paragraph("6,1"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+    
+        // column 2
+        page1Table
+        .getCell(0, 2)
+        .add(new Paragraph("0,2"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(2, 2)
+        .add(new Paragraph("2,2"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+    
+        page1Table
+        .getCell(5, 2)
+        .add(new Paragraph("5,2"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(6, 2)
+        .add(new Paragraph("6,2"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+    
+        // column 3
+        page1Table
+        .getCell(0, 3)
+        .add(new Paragraph("0,3"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(1, 3)
+        .add(new Paragraph("1,3"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(2, 3)
+        .add(new Paragraph("2,3"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(3, 3)
+        .add(new Paragraph("3,3"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(4, 3)
+        .add(new Paragraph("4,3"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(5, 3)
+        .add(new Paragraph("5,3"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+        page1Table
+        .getCell(6, 3)
+        .add(new Paragraph("6,3"))
+        .setVerticalAlign(VerticalAlign.CENTER);
+    
+         // column 4
+         page1Table
+         .getCell(3, 4)
+         .add(new Paragraph("3,4"))
+         .setVerticalAlign(VerticalAlign.CENTER);
+         page1Table
+         .getCell(4, 4)
+         .add(new Paragraph("4,4"))
+         .setVerticalAlign(VerticalAlign.CENTER);
+         
+    
+          //column 5
+          page1Table
+          .getCell(3, 5)
+          .add(new Paragraph("3,5"))
+          .setVerticalAlign(VerticalAlign.CENTER);
+          page1Table
+          .getCell(4, 5)
+          .add(new Paragraph("4,5"))
+          .setVerticalAlign(VerticalAlign.CENTER);
     
 
     const image1 = Media.addImage(doc, fs.readFileSync(obj.image1Url)); //var
@@ -78,7 +267,7 @@ const create = (obj)=>{
                 children: [
                     new Paragraph({
                         children: [
-                            new TextRun("Lorem Ipsum Foo Bar                                  "),  //var
+                            new TextRun(`${obj.page1.date}                                  `),  //var
                             new TextRun("Hello World                                          "),  //var
                             new TextRun("Hello World") //var
                         ],
@@ -100,7 +289,19 @@ const create = (obj)=>{
             new Paragraph(""),
             new Paragraph(""),
             new Paragraph(""),
-            new Paragraph(obj.page1.bodyText), //var
+            new Paragraph({
+                children: [image3, image4],
+                alignment: AlignmentType.CENTER,
+            }),
+            new Paragraph({
+                text: obj.page1.bodyText,  //var
+                alignment: AlignmentType.CENTER,
+                heading	: HeadingLevel.TITLE
+            }),
+            new Paragraph(""),
+            new Paragraph(""),
+            new Paragraph(""),
+            page1Table
         ],
     });
 }
