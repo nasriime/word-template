fcc0be4d65c2beb77b0567bcd4668f383fa165bd
Mohamed  NasrAllah <mnasr4040@gmail.com>
2019-09-30 12:23:51 +0200

    update date and spacing in footer

--- footer.js
+++ footer.js
@@ -1,29 +1,37 @@
-const { Footer, Paragraph, TextRun } = require("docx");
+const { Footer, Paragraph, TextRun } = require('docx');
 
-const createFooter=(obj)=>{
-    return {
-        default: new Footer({
-            children: [
-                new Paragraph({
-                    children: [
-                        new TextRun("2019-09-11                                  "), 
-                        new TextRun("NOKIA Confidential                                 "), 
-                        new TextRun("Page").pageNumber(),
-                        new TextRun("Total").numberOfTotalPages()
-                    ],
-                    border: {
-                        top: {
-                            color: "auto",
-                            space: 1,
-                            value: "single",
-                            size: 6,
-                        }
-                    },
-                })            
-            ],
+const createFooter = obj => {
+  const getDate = () => {
+    const today = new Date();
+    const date = `${today.getFullYear()}-${today.getMonth() +
+      1}-${today.getDate()}`;
+    return date;
+  };
+
+  return {
+    default: new Footer({
+      children: [
+        new Paragraph({
+          children: [
+            new TextRun(`${getDate()}                                  `),
+            new TextRun(
+              `NOKIA Confidential                                        `,
+            ),
+            new TextRun('Page').pageNumber(),
+            new TextRun('Total').numberOfTotalPages(),
+          ],
+          border: {
+            top: {
+              color: 'auto',
+              space: 1,
+              value: 'single',
+              size: 6,
+            },
+          },
         }),
-    }
-}
-
+      ],
+    }),
+  };
+};
 
 module.exports = createFooter;
